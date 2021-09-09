d82c24ede763d7c651a8eaf9c74b3962882d1612
Patrick <74840936+patllamas@users.noreply.github.com>
2021-09-09 13:40:00 -0400

    added a switch cases for messages this time

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ const client = new Discord.Client({
     intents: ["GUILDS", "GUILD_MESSAGES"]
 });
 
+// change this prefix const for command prefix
 const prefix = '%';
 
 client.once('ready', () => {
     console.log('bot is online!');
 })
 
+// default stuff
 client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
@@ -21,7 +23,16 @@ client.on('message', message => {
     }
 });
 
+// switch cases for less if statements
 client.on('message', message => {
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    switch(command) {
+        case 'ping2':
+            message.channel.send('this is 2nd ping');
+            break;
+    }
 
 })
 
